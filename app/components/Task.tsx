1d60bdd399d35ccb0ce00bc8fae07c85afa3e27d
Binaya <binayaaadhikari13687@gmail.com>
2025-12-04 18:06:30 +0545

    add task input box styled

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -4,7 +4,7 @@ const Task = ({content, id}: {content: string, id:string}) => {
     return (
         <div>
             <div>
-                <h1>{content}</h1>
+                <h1 className="text-black text-sm">{content}</h1>
                 <button onClick={() => removeTask(id)}>x</button>
             </div>
         </div>
--- app/components/ToDoList.tsx
+++ app/components/ToDoList.tsx
@@ -20,21 +20,25 @@ const ToDoList = ({tasks}: {tasks: task[]}) => {
     }
     return (
         <>
-            <div>
-                <div><form onSubmit={handleSubmit}>
+            <div className="flex flex-col items-center  bg-white w-screen h-100 pt-20 gap-5">
+                <h1 className="text-black text-4xl align-left justify-self-start font-sans font-bold flex">Your to Do</h1>
+                <div>
+                    <form onSubmit={handleSubmit} className="flex flex-row gap-5"> 
                         <input type="text"
+                        placeholder="Add new task"
                         value={newTask.task}
                         onChange={(e) => {
                             setNewTask(() => ({task: e.target.value}))
-                        }}/>
+                        }} className="text-neutral-500 p-4 w-100 h-8 bg-black-50 border-b-2 border-black"/>
 
-                        <button type="submit">+</button>
+                        <button type="submit" className="text-amber-50 w-10 h-1=8 bg-neutral-600 rounded-xl text-3xl">+</button>
                     </form>
                 </div>
-                <div>
+                <div className="text-black bg-amber-50 w-200 ">
                     {tasks.map(task => (
                         <Task key={task.id} content={task.task} id={task.id}/>
                     ))}
+                    
                 </div>
             </div>
         </>
