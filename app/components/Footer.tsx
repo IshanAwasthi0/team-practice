acc6bb6c735bd186199bb091d413fb8e64855298
siddhartha manandhar <playzsidz@gmail.com>
2025-12-04 13:59:08 +0545

    added icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,4 @@
-
+import { Icon } from '@iconify/react';
 
 const Footer = () => {
     return (
@@ -6,12 +6,17 @@ const Footer = () => {
  <div className="flex justify-center items-center bg-white">
   <div className="flex gap-40 text-xl text-black"> 
     <div>
-    <p>Your To Do List</p>
+    <p className="text-black font-bold">Your To Do List</p>
+    <div className="text-gray-400  text-sm mt-5 space-y-3">
+        <p>Explore our amazing features for free</p>
+        <p>Enjoy finishing your tasks in time</p>
+        <p>have no work left pending</p>
+    </div>
     </div>
 
     <div>
-    <p className="text-black font-bold text-center">About Us</p>
-    <div className="text-black text-sm mt-5 space-y-3">
+    <p className="text-black font-bold ">About Us</p>
+    <div className="text-gray-400  text-sm mt-5 space-y-3">
     <p>About this app</p>   
     <p>Contact us</p>
     <p>Features</p>
@@ -20,10 +25,13 @@ const Footer = () => {
  
 
     <div>
-    <p className="text-black font-bold text-center">Get In Touch</p>
-    <div className="text-black text-sm mt-5 space-y-3">
+    <p className="text-black font-bold ">Get In Touch</p>
+    <div className="text-gray-400 text-sm mt-5 space-y-3   ">
         <p>Questions or Feedbacks?</p>
         <p>We would love to hear from you</p>
+        <Icon icon="mdi:twitter" width="24" height="24" />
+        <Icon icon="iconoir:instagram" width="24" height="24" />
+        <Icon icon="ic:outline-facebook" width="24" height="24" />
     </div>
 
     </div>
