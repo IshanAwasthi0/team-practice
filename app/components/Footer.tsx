6b5a13ef3fa0b99d24fd5faab34c9a83f8072a0e
siddhartha manandhar <playzsidz@gmail.com>
2025-12-04 13:45:21 +0545

    finished about us section

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,29 @@
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <>
+        <div className="flex justify-center items-center bg-white">
+  <div className="flex gap-40 text-xl text-black"> 
+    <div
+    ><p>Your To Do List</p>
+    <p></p>
+    </div>
+
+    <div>
+    <p className="text-black font-bold text-center">About Us</p>
+    <div className="text-black text-sm mt-5 space-y-3">
+    <p>About this app</p>   
+    <p>Contact us</p>
+    <p>Features</p>
+    </div>
+    </div>
+ 
+
+    <div><p>Get In Touch</p></div>
+  </div>
+</div>
+        
+        </> 
     )
 }
-
 export default Footer
